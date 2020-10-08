099517dd5383b31f52ad18478afa6db8a39287a7
Rajan Phadnis <rajansd28@gmail.com>
2020-10-08 11:59:49 -0700

    working on convolution

--- Team_project.py
+++ Team_project.py
@@ -2,7 +2,7 @@
 ===============================================================================
 ENGR 133 Fa 2020
 Assignment Information
-	Assignment:     Py4 Task 4
+	Assignment:     Python Project
 	Author:         Tejas Chandrasekhar, tlchandr@purdue.edu
                     Zachary Chen, chen3652@purdue 
                     Rajan Phadnis, rphadnis@purdue 
@@ -20,19 +20,58 @@ Assignment Information
 	have to list that person as a contributor here as well.
 ===============================================================================
 '''
-import numpy
+import numpy as np
 import matplotlib as lib
 import matplotlib.pyplot as plt
-file_name = input("filename: ") #takes in a picture from the user who inputs the name of a file
-variable = lib.image.imread(file_name) #matplot will read the image into an array
-def dotproduct(color): #dotproduct will convert image into greyscale
-    dot = numpy.dot(color[...,:3], [0.2989, 0.5870, 0.1140]) #Formula for converting into greyscale, takes each term and calculates dotproduct
+# file_name = input("filename: ") #takes in a picture from the user who inputs the name of a file
+variable = lib.image.imread("Coins.PNG") #matplot will read the image into an array
+def greyscaleConverter(colorMatrix): #dotproduct will convert image into greyscale
+    dot = np.dot(colorMatrix[...,:3], [0.2126, 0.7152, 0.0722]) #Formula for converting into greyscale, takes each term and calculates dotproduct
     return dot #returns dot product
+def convolution(imageMatrix, convoluteMatrix):
+    pad_length = (len(convoluteMatrix[0])-1)/2
+    paddedImage = np.pad(imageMatrix, int(pad_length), mode="constant")
+    finalImage = imageMatrix
+    # print(imageMatrix)
+    # print(paddedImage)
+    # print(sk)
+    
+    # return paddedImage
+    for row in imageMatrix:
+        for pixel in row:
+            pixelIndex = np.where(row == pixel)[0]+1
+            rowIndex = np.where(imageMatrix == row)[0]+1
+            sumOfConvolves = 0
+            kernel = convoluteMatrix
+            x=0
+            y=0
+            
+            for krow in kernel:
+                for kpixel in krow:
+                    print(rowIndex[0])
+                    # print(krow)
+                    kernel[y][x] = paddedImage[rowIndex[0]+y][pixelIndex[0]+x]
+                    x = x +1
+                y=y+1
+            print(kernel)  
+            # for i in range(0, len(convoluteMatrix)):
+            #     print(sumOfConvolves)
+            #     sumOfConvolves += np.convolve(kernel, convoluteMatrix)
+            finalImage[rowIndex-1][pixelIndex-1] = sumOfConvolves
+                
+    # print(finalImage)
+                
+            
+            
+        
+             
+sk = [[1,1,1],[1,1,1],[1,1,1]]
 
-greyscalematrix = dotproduct(variable)
-
-plt.imshow(greyscalematrix, cmap=plt.get_cmap('gray'), vmin=0, vmax=1)
-plt.show()
+    
+greyscalematrix = greyscaleConverter(variable)
+blurred = convolution(greyscalematrix,sk)
+# plt.imshow(blurred, cmap=plt.get_cmap('gray'), vmin=0, vmax=1)
+# plt.show()
 
 
 '''
